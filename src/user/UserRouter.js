7c03a78c1987e1794a56a9139638ab1eba184cb0
Salim Hossain <salimh5757@gmail.com>
2021-11-01 00:48:30 +0600

    used express validator

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,20 +1,22 @@
 const express = require('express');
 // const bcrypt = require('bcryptjs');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 // const User = require('./User');
 const UserService = require('./UserService');
 
-router.post('/api/1.0/users', async (req, res) => {
-  const user = req.body;
-  if(user.username === null){
-    return res.status(400).send({validationErrors: {
-      username: 'Username cannot be null'
-    }});
+router.post('/api/1.0/users',
+check('username').notEmpty().withMessage('Username cannot be null'),
+check('email').notEmpty().withMessage('Email cannot be null'), 
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    const validationErrors = {};
+    errors.array().forEach(error => (validationErrors[error.param] = error.msg));
+    return res.status(400).send({validationErrors: validationErrors});
   }
+
   await UserService.save(req.body);
-  //   const hash = await bcrypt.hash(req.body.password, 10);
-  //   const user = { ...req.body, password: hash };
-  //   await User.create(user);
   return res.status(200).send({ message: 'User created' });
 });
 
